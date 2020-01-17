f061b3405e2b2ff5905b53ac1f6e86b5a67b31b1
Alvin Tang <alvintang@gmail.com>
2020-01-17 22:41:52 +0800

    added simple css. on-off LED on click

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask import Response
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,3 +23,15 @@ def hello(name=None):
 @app.route('/controller/')
 def controller(name=None):
     return render_template('rc.html')
+
+@app.route('/controller/<direction>', methods=['POST'])
+def controller_dir(direction=None):
+    print(direction)
+    if direction == "forward":
+        GPIO.output(18, GPIO.HIGH)
+    elif direction == "backward":
+        GPIO.output(18, GPIO.HIGH)
+    elif direction == "off":
+        GPIO.output(18, GPIO.LOW)
+
+    return Response("{\"direction\":\""+direction+"\"}", status=200, mimetype='application/json') 
